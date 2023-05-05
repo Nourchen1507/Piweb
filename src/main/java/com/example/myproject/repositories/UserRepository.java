107d839e946b68149b79ce7e8a42c1a7ff8a92f9
ahmedmahfoudh1920 <ahmed.mahfoudh@esprit.tn>
2023-05-06 00:16:43 +0100

    maj

--- src/main/java/com/example/myproject/repositories/UserRepository.java
+++ src/main/java/com/example/myproject/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.myproject.repositories;
 
 import com.example.myproject.entities.User;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
